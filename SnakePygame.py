ec066366b1488a04a7ac6cd2a2cc20b4a76d101f
Malik-hc <82786837+Malik-hc@users.noreply.github.com>
2024-02-14 18:37:53 -0500

    Create ability to quit screen

--- SnakePygame.py
+++ SnakePygame.py
@@ -1,12 +1,18 @@
 import pygame
 
 pygame.init()
+
 screen = pygame.display.set_mode((500,500))
+
 pygame.display.update()
 pygame.display.set_caption("SNAKE")
+
 game_over = False
+
 while not game_over:
     for event in pygame.event.get():
-        print (event) 
+        if event.type == pygame.QUIT:
+            game_over = True
+            
 pygame.quit()
 quit()
